d3d9402484f0fbdc450dde07f27db947c0be9caf
anastasija-grekova <125025406+anastasija-grekova@users.noreply.github.com>
2023-03-04 23:20:02 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -43,9 +43,7 @@ def main():
                 parents = newLines[1].split()
                 parents = [int(i) for i in parents] 
                 print(compute_height(n, parents))  
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-
+    
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
