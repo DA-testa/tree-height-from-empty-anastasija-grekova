c300382950262b95fe1c276b587e2ca36c2a76a5
anastasija-grekova <125025406+anastasija-grekova@users.noreply.github.com>
2023-03-04 20:04:52 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -2,26 +2,80 @@
 
 import sys
 import threading
-import numpy
+#import csv
+#import numpy
+import os
 
 
 def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
-
+    max_height = [1] * n
+    for i, j in enumerate(parents):
+        while j != -1:
+            max_height[i] += 1
+            j = parents[j]
+    return max(max_height)
 
 def main():
-    # implement input form keyboard and from files
+    check = input()
+    if check == "I":
+        n = int(input())
+        parents = list(map(int, input().split()))
+
+    if check == 'F':
+        path = os.getcwd() + '/test'
+        os.chdir(path)
+        file_name = input()
+
+        if 'a' in file_name:
+            print("error")
+            quit()
+        else:
+            file_path = f"{path}/{file_name}"
+            #print(file_path)
+            with open(file_path, "r", encoding="utf-8-sig") as f:
+                #    data = f.read()
+                #for line in lines:
+                lines = f.readlines()
+                newLines = []
+                for x in lines:
+                    newLines.append(x.replace("\n", ""))
+                #for i in range(0, len(newLines)):
+                #    newLines[i] = int(newLines[i])
+                #    newLines.append(int(x))
+                n = int(newLines[0])
+                parents = newLines[1].split()
+                parents = [int(i) for i in parents]
+                #for i in enumerate(parents):
+                #    parents[i] = int(parents[i])
+                #print(parents)
+                    #for line in lines:
+                    #    count += 1
+                    #    n = lines.format(1, line.strip())
+                    #    parents = lines.format
+                    #if countLines == 1 :
+                #        print(f.readline())
+                        
+                    #    n = f.readline;
+                    #    print(n)
+                #    else:
+                        #print(f.readline())
+                #        parents = f.readline()
+                #    countLines += 1
+        #else:
+        #    print("sorry!")
     
+    print(compute_height(n, parents))
+    
+        #print(text)
+
+
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
     
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
-    pass
+    #pass
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
@@ -29,5 +83,5 @@ def main():
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
-main()
+#main()
 # print(numpy.array([1,2,3]))
