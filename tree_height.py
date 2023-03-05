fc5890933c3b13e96c2969019e832b6a32bd1e5e
anastasija-grekova <125025406+anastasija-grekova@users.noreply.github.com>
2023-03-05 19:33:53 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -2,6 +2,7 @@
 
 import sys
 import threading
+#import csv
 #import numpy
 import os
 
@@ -12,6 +13,7 @@ def compute_height(n, parents):
         while j != -1:
             max_height[i] += 1
             j = parents[j]
+    #out_queue.put(max)
     return max(max_height)
 
 def main():
@@ -22,6 +24,7 @@ def main():
     if check == 'I':
         n = int(input())
         parents = list(map(int, input().split()))
+        
         print(compute_height(n, parents))
 
     if check == 'F':
@@ -34,14 +37,19 @@ def main():
         else:
             file_path = f"{path}/{file_name}"
             with open(file_path, "r", encoding="utf-8-sig") as f:
-                lines = f.readlines()
-                newLines = []
-                for x in lines:
-                    newLines.append(x.replace("\n", ""))
-                n = int(newLines[0])
-                parents = newLines[1].split()
+                newLines = f.readlines()
+            #    newLines = []
+            #    for x in lines:
+           #         newLines.append(x.replace("\n", ""))
+                n = int(newLines[0].replace("\n", ""))
+                parents = newLines[1].replace("\n", "").split()
                 parents = [int(i) for i in parents] 
-                print(compute_height(n, parents))  
+              #  out_queue=queue.Queue()
+                #t1=threading.Thread(target=compute_height, args=(n, parents, out_queue))
+                #t1.start()
+                #t1.join()
+                
+            print(compute_height(n, parents))  
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
 
@@ -49,7 +57,7 @@ def main():
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
 sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
+threading.stack_size(2**30)   # new thread will get stack of such size
 threading.Thread(target=main).start()
 #main()
 # print(numpy.array([1,2,3]))
